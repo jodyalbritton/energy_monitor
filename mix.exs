123326f60d810d7cf8822d3556fab3e997e27d8e
Jody Albritton <jody@eml.cc>
2018-01-10 06:04:25 -0800

    update stex

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule EnergyMonitor.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:stex, "~> 0.2.3"},
+      {:stex, "~> 0.2.4"},
     ]
   end
 
